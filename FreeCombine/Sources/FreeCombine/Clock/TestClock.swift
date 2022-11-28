32f477f598a8042b48b8defebf6c1fd20bb09f81
Van Simmons <789213+rvsrvs@users.noreply.github.com>
2022-11-27 19:46:01 -0500

    Add copyright notices
    Signed-off-by: Van Simmons <789213+rvsrvs@users.noreply.github.com>

--- FreeCombine/Sources/FreeCombine/Clock/TestClock.swift
+++ FreeCombine/Sources/FreeCombine/Clock/TestClock.swift
@@ -3,14 +3,28 @@
 //  
 //
 //  Created by Van Simmons on 11/26/22.
-//  Inspired the PointFree version of the same.
-//  Differences:
-//  1. Lock-free
-//  2. No "megaYield"
-//  3. No AsyncStream<Never>
-//  4. No Foundation, uses Atomics instead
-//  4. Uses composability tools from FreeCombine
+//  Copyright 2022, ComputeCycles, LLC
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+/*:  Inspired the PointFree version of the same.
+  Differences:
+  1. Lock-free
+  2. No "megaYield"
+  3. No AsyncStream<Never>
+  4. No Foundation, uses Atomics instead
+  4. Uses composability tools from FreeCombine
+ */
 #if swift(>=5.7)
 import Atomics
 
--- FreeCombine/Sources/FreeCombine/Future/Future.swift
+++ FreeCombine/Sources/FreeCombine/Future/Future.swift
@@ -4,12 +4,25 @@
 //
 //  Created by Van Simmons on 9/5/22.
 //
-
+//  Copyright 2022, ComputeCycles, LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 /*: Modelled after EventLoopFuture from NIO
  [ELF's should be lock-free](https://github.com/apple/swift-nio/blob/26afcecdc2142f1cd0d9b7f4d25b3a72938c3368/Sources/NIOCore/EventLoopFuture.swift#L379)
 
  Note that a major difference between ELF and Future is that
- every Future creates a Cancellable as a result of it's `sink` call.
+ every Future creates a Cancellable as a result of its `sink` call.
  It is the Cancellable that is a reference type while Future is a struct.
  It is the Cancellable that is not allowed to leak.
 
