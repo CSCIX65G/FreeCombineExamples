968544155c2172cee4348879bf7324eecc05c256
Van Simmons <789213+rvsrvs@users.noreply.github.com>
2022-12-11 18:59:28 -0500

    Separating out Debouncer type
    Signed-off-by: Van Simmons <789213+rvsrvs@users.noreply.github.com>

--- ComposableConcurrency/Sources/Publisher/Operators/Time/Debounce.swift
+++ ComposableConcurrency/Sources/Publisher/Operators/Time/Debounce.swift
@@ -24,53 +24,82 @@ import Queue
 
 @available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
 extension Publisher {
+    public struct Debouncer<C: Clock> {
+        let duration: C.Duration
+        let downstream: Downstream
+        let downstreamState: ManagedAtomic<Box<DownstreamState>>
+        let downstreamValue: ManagedAtomic<Box<(instant: C.Instant, value: Output)>?>
+        let queue: Queue<Void>
+        private(set) var sender: Cancellable<Void>!
+
+        init(
+            clock: C,
+            duration: C.Duration,
+            downstream: @escaping Downstream,
+            downstreamState: ManagedAtomic<Box<DownstreamState>> = .init(.init(value: .init())),
+            downstreamValue: ManagedAtomic<Box<(instant: C.Instant, value: Output)>?> = .init(.none),
+            queue: Queue<Void> = .init(buffering: .bufferingNewest(1))
+        ) async {
+            self.duration = duration
+            self.downstream = downstream
+            self.downstreamState = downstreamState
+            self.downstreamValue = downstreamValue
+            self.queue = queue
+            await unfailingPause { resumption in
+                self.sender = .init {
+                    resumption.resume()
+                    for await _ in queue.stream {
+                        var toSendOptional: Output? = .none
+                        while let (then, r) = downstreamValue.exchange(.none, ordering: .sequentiallyConsistent)?.value {
+                            toSendOptional = r
+                            guard clock.now < then.advanced(by: duration) else { break }
+                            try await clock.sleep(until: then.advanced(by: duration), tolerance: .none)
+                        }
+                        guard let toSend = toSendOptional else { continue }
+                        do { try await downstream(.value(toSend)) }
+                        catch {
+                            _ = downstreamState.exchange(.init(value: .init(errored: error)), ordering: .sequentiallyConsistent)
+                            queue.finish()
+                            for await _ in queue.stream { }
+                            return
+                        }
+                    }
+                }
+            }
+        }
+
+        func send(now: C.Instant, value: Output) throws {
+            _ = downstreamValue.exchange(.init(value: (now, value)), ordering: .sequentiallyConsistent)
+            try queue.tryYield()
+        }
+    }
+}
+
+@available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
+extension Publisher {
     func debounce<C: Clock>(
         clock: C,
         duration: Swift.Duration
     ) -> Self where C.Duration == Swift.Duration {
         .init { resumption, downstream in
-            let downstreamState = ManagedAtomic<Box<DownstreamState>>.init(.init(value: .init()))
-            let downstreamValue = ManagedAtomic<Box<(instant: C.Instant, value: Output)>?>.init(.none)
-            let queue = Queue<Void>(buffering: .bufferingNewest(1))
-            let sender: MutableBox<Cancellable<Void>?> = .init(value: .none)
+            let debouncerBox: MutableBox<Debouncer<C>?> = .init(value: .none)
 
             return self(onStartup: resumption) { r in
-                try await Self.check(downstreamState, sender.value)
-                if sender.value == nil {
-                    let _: Void = try await pause { resumption in
-                        sender.set(
-                            value: .init {
-                                resumption.resume()
-                                for await _ in queue.stream {
-                                    var toSendOptional: Output? = .none
-                                    while let (then, r) = downstreamValue.exchange(.none, ordering: .sequentiallyConsistent)?.value {
-                                        toSendOptional = r
-                                        guard clock.now < then.advanced(by: duration) else { break }
-                                        try await clock.sleep(until: then.advanced(by: duration), tolerance: .none)
-                                    }
-                                    guard let toSend = toSendOptional else { continue }
-                                    do { try await downstream(.value(toSend)) }
-                                    catch {
-                                        _ = downstreamState.exchange(.init(value: .init(errored: error)), ordering: .sequentiallyConsistent)
-                                        queue.finish()
-                                        for await _ in queue.stream { }
-                                        return
-                                    }
-                                }
-                            }
-                        )
-                    }
+                if debouncerBox.value == nil {
+                    await debouncerBox.set(value: .init(clock: clock, duration: duration, downstream: downstream))
                 }
+                guard let debouncer = debouncerBox.value else { fatalError("Failed to create debouncer") }
+                try await Self.check(debouncer.downstreamState, debouncer.sender)
 
                 switch r {
                     case .completion:
-                        _ = try? queue.tryYield()
-                        queue.finish()
-                        try await sender.value?.value ?? ()
+                        _ = try? debouncer.queue.tryYield()
+                        debouncer.queue.finish()
+                        try await debouncer.sender.value
                         return try await downstream(r)
                     case let .value(value):
-                        _ = downstreamValue.exchange(.init(value:(clock.now, value)), ordering: .sequentiallyConsistent)
-                        guard case .terminated = queue.yield() else { return }
+                        _ = debouncer.downstreamValue.exchange(.init(value:(clock.now, value)), ordering: .sequentiallyConsistent)
+                        guard case .terminated = debouncer.queue.yield() else { return }
                         throw FinishedError()
                 }
             }
