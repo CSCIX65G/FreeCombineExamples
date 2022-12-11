c23870ef2eb69071c627cfcb6e662047be1258bb
Van Simmons <789213+rvsrvs@users.noreply.github.com>
2022-12-11 18:48:17 -0500

    Simplify debounce
    Signed-off-by: Van Simmons <789213+rvsrvs@users.noreply.github.com>

--- ComposableConcurrency/Sources/Publisher/Operators/Time/Debounce.swift
+++ ComposableConcurrency/Sources/Publisher/Operators/Time/Debounce.swift
@@ -24,66 +24,44 @@ import Queue
 
 @available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
 extension Publisher {
-
-    private func debounceHandler<C: Clock>(
-        resumption: Resumption<Void>,
-        clock: C,
-        duration: C.Duration,
-        downstream: @escaping Downstream,
-        isDispatchable: ManagedAtomic<Box<DownstreamState>>,
-        atomicPair: ManagedAtomic<Box<(instant: C.Instant, value: Output)>?>,
-        queue: Queue<Void>
-    ) -> @Sendable () async throws -> Void where C.Duration == Swift.Duration {
-        {
-            resumption.resume()
-            for await _ in queue.stream {
-                var toSendOptional: Output? = .none
-                while let (then, r) = atomicPair.exchange(.none, ordering: .sequentiallyConsistent)?.value {
-                    toSendOptional = r
-                    guard clock.now < then.advanced(by: duration) else { break }
-                    try await clock.sleep(until: then.advanced(by: duration), tolerance: .none)
-                }
-                guard let toSend = toSendOptional else { continue }
-                do { try await downstream(.value(toSend)) }
-                catch {
-                    _ = isDispatchable.exchange(.init(value: .init(errored: error)), ordering: .sequentiallyConsistent)
-                    queue.finish()
-                    for await _ in queue.stream { }
-                    return
-                }
-            }
-        }
-    }
-
     func debounce<C: Clock>(
         clock: C,
         duration: Swift.Duration
     ) -> Self where C.Duration == Swift.Duration {
         .init { resumption, downstream in
-            let isDispatchable = ManagedAtomic<Box<DownstreamState>>.init(.init(value: .init()))
-            let atomicPair = ManagedAtomic<Box<(instant: C.Instant, value: Output)>?>.init(.none)
+            let downstreamState = ManagedAtomic<Box<DownstreamState>>.init(.init(value: .init()))
+            let downstreamValue = ManagedAtomic<Box<(instant: C.Instant, value: Output)>?>.init(.none)
             let queue = Queue<Void>(buffering: .bufferingNewest(1))
             let sender: MutableBox<Cancellable<Void>?> = .init(value: .none)
 
             return self(onStartup: resumption) { r in
-                try await Self.check(isDispatchable, sender.value)
+                try await Self.check(downstreamState, sender.value)
                 if sender.value == nil {
                     let _: Void = try await pause { resumption in
                         sender.set(
-                            value: .init(
-                                operation: debounceHandler(
-                                    resumption: resumption,
-                                    clock: clock,
-                                    duration: duration,
-                                    downstream: downstream,
-                                    isDispatchable: isDispatchable,
-                                    atomicPair: atomicPair,
-                                    queue: queue
-                                )
-                            )
+                            value: .init {
+                                resumption.resume()
+                                for await _ in queue.stream {
+                                    var toSendOptional: Output? = .none
+                                    while let (then, r) = downstreamValue.exchange(.none, ordering: .sequentiallyConsistent)?.value {
+                                        toSendOptional = r
+                                        guard clock.now < then.advanced(by: duration) else { break }
+                                        try await clock.sleep(until: then.advanced(by: duration), tolerance: .none)
+                                    }
+                                    guard let toSend = toSendOptional else { continue }
+                                    do { try await downstream(.value(toSend)) }
+                                    catch {
+                                        _ = downstreamState.exchange(.init(value: .init(errored: error)), ordering: .sequentiallyConsistent)
+                                        queue.finish()
+                                        for await _ in queue.stream { }
+                                        return
+                                    }
+                                }
+                            }
                         )
                     }
                 }
+
                 switch r {
                     case .completion:
                         _ = try? queue.tryYield()
@@ -91,7 +69,7 @@ extension Publisher {
                         try await sender.value?.value ?? ()
                         return try await downstream(r)
                     case let .value(value):
-                        _ = atomicPair.exchange(.init(value:(clock.now, value)), ordering: .sequentiallyConsistent)
+                        _ = downstreamValue.exchange(.init(value:(clock.now, value)), ordering: .sequentiallyConsistent)
                         guard case .terminated = queue.yield() else { return }
                         throw FinishedError()
                 }
